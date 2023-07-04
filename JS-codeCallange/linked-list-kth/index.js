6809cb00ad8e631e146e3ec92f22d8219aefa9fe
Ayah-AQ <ayoya_1998@hotmail.com>
2023-07-04 20:45:33 +0000

    kth

--- JS-codeCallange/linked-list-kth/index.js
+++ JS-codeCallange/linked-list-kth/index.js
@@ -1,34 +1,44 @@
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  kthFromEnd(k) {
-    if (this.head === null) {
-      throw new Error("LinkedList is empty.");
-    }
-
-    let count = 0;
-    let current = this.head;
-    let kthNode = null;
-
-    
-    while (current !== null) {
-      if (count === k) {
-        kthNode = this.head;
-      } else if (count > k) {
-        kthNode = kthNode.next;
-      }
-      count++;
-      current = current.next;
-    }
-
-    if (kthNode === null) {
-      throw new Error("k is larger than the size of the LinkedList.");
-    }
-
-    return kthNode.value;
+ class LinkedList {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
 
-module.exports = LinkedList;
+
+ function getNodeFromEnd(head, n) {
+  if (head === null) {
+    return null;
+  }
+
+  if (n <= 0) {
+    throw new Error("Invalid value: n = " + n);
+  }
+
+  let length = 0;
+  let now = head;
+
+  while (now !== null) {
+    length++;
+    now = now.next;
+  }
+
+  if (n > length) {
+    throw new Error(n + " is greater than the number in the list");
+  }
+
+  let steps = length - n;
+  now = head;
+  while (steps > 0) {
+    now = now.next;
+    steps--;
+  }
+
+  return now;
+}
+
+
+module.exports = {
+  LinkedList,
+  getNodeFromEnd,
+};
